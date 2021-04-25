b1c7b29cd2d451cca9734e46cd42fcff87a96120
Rafael Luiz <rafael.s.luiz@gmail.com>
2021-04-25 20:09:34 -0300

    Initial solution

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.AccountException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			Account ac = new Account(number, holder, initialBalance, withdrawLimit);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double amount = sc.nextDouble();
+			ac.withdraw(amount);
+			System.out.println("New balance: " + String.format("%.2f", ac.getBalance()));
+		}
+		catch (AccountException e) {
+			System.out.println("WithDrawn error: " + e.getMessage());
+		}
+		
+				
+		sc.close();
+
+	}
+
+}
